21303e14874d4f6f98cc6ab9f2c9095c95396be3
nathan <yes@4nathan.com>
2024-12-03 16:11:43 +0800

    增加log

--- app/bot/app.py
+++ app/bot/app.py
@@ -1,10 +1,11 @@
 from telegram.ext import Application
-
+from loguru import logger
 import os
 
 bot_token = os.environ.get("bot_token")
 if not bot_token:
     raise Exception("bot_token not found")
 
+logger.info("======Bot start=========")
 application = Application.builder().token(bot_token).build()
 
