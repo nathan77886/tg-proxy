b2742f458505226da5611de0f905b8c60c905920
nathan <yes@4nathan.com>
2024-12-03 17:47:44 +0800

    增加try

--- app/__init__.py
+++ app/__init__.py
@@ -12,10 +12,13 @@ async def on_ws_barrage_open(websocket: WebSocket, group_id: str):
     await websocket.accept()
     set_channel(group_id, websocket)
     logger.info(f"{group_id} connect watch")
-    while True:
-        data = await websocket.receive_text()
-        await asyncio.sleep(10)
-        await websocket.send_json({"type": "heartbeat"})
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await asyncio.sleep(10)
+            await websocket.send_json({"type": "heartbeat"})
+    except Exception as e:
+        logger.info(f"{group_id} disconnect watch")
 
 @app.get("/hello")
 async def hello():
