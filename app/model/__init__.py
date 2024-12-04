f1ea8e2885bcc856639432819e01cff5c346286f
nathan <yes@4nathan.com>
2024-12-04 15:11:32 +0800

    修改字符串类型

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -6,11 +6,13 @@ group_channel = {}
 
 
 def set_channel(group_id, channel: WebSocket):
-    if group_channel.get(group_id):
-        group_channel[group_id].append(channel)
+    g_id = str(group_id)
+    if group_channel.get(g_id):
+        group_channel[g_id].append(channel)
     else:
-        group_channel[group_id] = [channel]
+        group_channel[g_id] = [channel]
 
 
 def get_channel(group_id) -> List[WebSocket]:
-    return group_channel.get(group_id)
+    g_id = str(group_id)
+    return group_channel.get(g_id)
