8667223ee81363e8fcb60b357a14e5dbd2101208
nathan <yes@4nathan.com>
2024-12-17 17:38:49 +0800

    修改实例

--- app/bot/app.py
+++ app/bot/app.py
@@ -4,16 +4,9 @@ import os
 
 application = None
 
-def bot_init():
-    run_bot = os.environ.get("run_bot")
-    if not run_bot:
-        return
+bot_token = os.environ.get("bot_token")
+if not bot_token:
+    raise Exception("bot_token not found")
 
-    bot_token = os.environ.get("bot_token")
-    if not bot_token:
-        raise Exception("bot_token not found")
-
-    logger.info("======Bot start=========")
-    application = Application.builder().token(bot_token).build()
-
-bot_init()
+logger.info("======Bot start=========")
+application = Application.builder().token(bot_token).build()
