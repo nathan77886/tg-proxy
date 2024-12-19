d2b6c301b13b74aebc68b120f524a34bc6dc0a7d
nathan <yes@4nathan.com>
2024-12-19 17:30:26 +0800

    优化代码报错

--- app/model/room.py
+++ app/model/room.py
@@ -7,31 +7,29 @@ async def create_room(session_id, room_name=""):
     if room_name == "":
         ## 随机8位字符串
         room_name = "".join(random.sample(string.ascii_letters + string.digits, 8))
-    session = get_session()
-    room = session.query(RoomSessionMapping).filter(RoomSessionMapping.session_id == session_id).first()
-    if room is None:
-        ## 创建房间和映射
-        room = Room.create_room(room_name)
-        session.add(room)
-        session.commit()
-        room_session_mapping = RoomSessionMapping.create_room_session_mapping(room.id, session_id)
-        session.add(room_session_mapping)
-        session.commit()
-    session.close()
-    return room
+    with get_session() as session:
+        session = get_session()
+        room = session.query(RoomSessionMapping).filter(RoomSessionMapping.session_id == session_id).first()
+        if room is None:
+            ## 创建房间和映射
+            room = Room.create_room(room_name)
+            session.add(room)
+            session.commit()
+            room_session_mapping = RoomSessionMapping.create_room_session_mapping(room.id, session_id)
+            session.add(room_session_mapping)
+            session.commit()
+        return room
 
 
 async def get_room(room_name):
-    session = get_session()
-    room = session.query(Room).filter(Room.room_name == room_name).first()
-    session.close()
-    return room
+    with get_session() as session:
+        room = session.query(Room).filter(Room.room_name == room_name).first()
+        return room
 
 async def get_room_session(room_name):
-    session = get_session()
-    room = session.query(Room).filter(Room.room_name == room_name).first()
-    if room is None:
-        return None
-    room_session_mapping = session.query(RoomSessionMapping).filter(RoomSessionMapping.room_id == room.id).first()
-    session.close()
-    return room_session_mapping.session_id
+    with get_session() as session:
+        room = session.query(Room).filter(Room.room_name == room_name).first()
+        if room is None:
+            return None
+        room_session_mapping = session.query(RoomSessionMapping).filter(RoomSessionMapping.room_id == room.id).first()
+        return room_session_mapping.session_id
