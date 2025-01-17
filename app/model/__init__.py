a9d2588c170554255c401acb8992c0ef8b227bfe
nathan <yes@4nathan.com>
2025-01-17 17:34:54 +0800

    移除websocket 代理

--- app/model/__init__.py
+++ app/model/__init__.py
@@ -5,8 +5,6 @@ from collections import defaultdict
 import asyncio
 import json
 
-group_channel = {}
-
 # 用于存储每个 group_id 的队列和连接
 _message_queues: Dict[str, asyncio.Queue] = defaultdict(asyncio.Queue)
 
@@ -17,44 +15,8 @@ async def on_event_stream(group_id):
         message = await queue.get()
         yield json.dumps(message)
 
-
-def set_channel(group_id, channel: WebSocket, random_id):
-    g_id = str(group_id)
-    global group_channel
-    if not group_channel:  # type: ignore
-        group_channel = {}
-    if group_channel.get(g_id):
-        group_channel[g_id][random_id] = channel
-    else:
-        group_channel[g_id] = {random_id: channel}
-
-
-def remove_channel(group_id, random_id):
-    g_id = str(group_id)
-    global group_channel
-    if not group_channel:  # type: ignore
-        return
-    if group_channel.get(g_id) and group_channel[g_id].get(random_id):
-        del group_channel[g_id][random_id]
-
 # 派发事件
 async def dispatch(group_id, msg):
-    global group_channel
-    if not group_channel:  # type: ignore
-        logger.error("group channel is empty")
-        return
     g_id = str(group_id)
-    channel_map = group_channel.get(g_id)
-    if not channel_map:
-        logger.error(f"group {g_id} not found")
-        return
-    length = len(channel_map)
-    logger.info(f"dispatch msg to {g_id} {length}")
-    for ws in channel_map.values():
-        logger.info(f"dispatch msg to {group_id}")
-        try:
-            await ws.send_json({"type": "text", "data": msg})
-        except Exception as e:
-            logger.error(e)
-    
+    logger.info(f"dispatch msg to {g_id}")
     await _message_queues[group_id].put(msg)
