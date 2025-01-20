3fea37c8c829563d3af6308485f27876e14be350
nathan <yes@4nathan.com>
2025-01-20 19:58:14 +0800

    屏蔽hello的log

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from app import app, run_bot
 from uvicorn import run
 from threading import Thread
+import logging
 
+# 创建一个自定义日志过滤器
+class RequestFilter(logging.Filter):
+    def filter(self, record):
+        # 过滤掉 /healthy 路径的日志
+        if "/hello" in record.getMessage():
+            return False
+        return True
+
+# 配置日志
+logger = logging.getLogger("uvicorn.access")
+handler = logging.StreamHandler()
+handler.addFilter(RequestFilter())  # 添加过滤器
+logger.addHandler(handler)
 
 def start_app():
     run(app, host="0.0.0.0", port=5000)
